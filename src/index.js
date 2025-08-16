99af61a62d56df99c65b67f55465d5ad4c894d3d
shree-05 <shreevyapari@gmail.com>
2025-08-16 16:35:18 +0530

    mongoDB connection

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// import mongoose from 'mongoose'
+// import { DB_NAME } from './constants'
+
+import dotenv from 'dotenv'
+import connectDB from './db/index.js'
+
+dotenv.config({
+    path :'./env'
+})
+
+
+connectDB()
+
+
+
+/*
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    } catch (error) {
+        console.error("ERROR ", error)
+        throw err
+    }
+})()
+*/
